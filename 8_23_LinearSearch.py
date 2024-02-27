8717413bfd70523d1f412ef8cf00617c7cd23be2
bayleaflet <bayleejchristensen@gmail.com>
2024-02-27 15:06:37 -0800

    Accessibility

--- 8_23_LinearSearch.py
+++ 8_23_LinearSearch.py
@@ -1,3 +1,6 @@
+# BJC, 8/23/23
+# Original Author
+
 A = [10,3,7,1,12]
 
 #Linear Search Algorithm
@@ -7,7 +10,7 @@ def FindItem(A,x):
         if A[i] == x:
             return i
         return -1
-    #return A.index(x) # Apparently this could work
+    return A.index(x)
 
-# Binary Search, could throw out half the search .
-B = [2,3,6,7,9,14,18,19,23]
+
+
